27f50123903e47a4b4da71488f918c0f3368347d
Uzairimam03 <uzair.imam03@gmail.com>
2026-03-29 21:23:20 +0500

    Changes the folder structure

---
+++ js-modules/index.js
@@ -0,0 +1,13 @@
+// console.log("Hello, World!");
+
+// // console.log(window)
+// // windows is not defined in node.js, it is only defined in browsers.
+
+
+const {add, sub}  = require ("./math");
+
+// console.log("Math value is", add(2,3 ));
+
+console.log("Math sub  value is", sub(2,3 ));
+
+console.log("Math add  value is", add(2,3 ));
