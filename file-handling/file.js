472cf6f73e1595479e2ef9514021b70399442c28
Uzairimam03 <uzair.imam03@gmail.com>
2026-03-31 12:23:16 +0500

    learned http-server

--- file-handling/file.js
+++ file-handling/file.js
@@ -5,10 +5,13 @@ const fs = require ("fs")
 
 // fs.writeFileSync("./test.txt", "Hey there is a test")
 
+// const result =  fs.readFileSync("./test.txt")
+// console.log(result)
+
 //  fs.appendFileSync("./test.txt", `${new Date().toLocaleString()}, Hey There\n` )
 
 // fs.cpSync("./test.txt", "newtest.txt")
 
-fs.unlinkSync("./newtest.txt")
+// to delete ==> fs.unlinkSync("./newtest.txt")
 
 
